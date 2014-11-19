6b81fb0e210b88afee88cb979bf84d62eb6b4566
Javier Lopez <fjlopez@gsyc.es>
2014-11-19 22:58:59 +0100

    Allow target arch & release selection in script build.sh
    Change-Id: I309031d0b6066ee76358ffa40b2b497a6d72447b

--- build.sh
+++ build.sh
@@ -25,6 +25,39 @@ function end {
   exit 0;
 }
 
+function usage {
+    echo "Usage:"
+    echo ""
+    echo "      build.sh [ -t, --target_arch (arm|arm64|ia32|x64|mipsel) ] [ -r, --revision rev_number ]"
+    echo ""
+}
+
+# Get input parameters
+while [ "$1" != "" ]; do
+    case $1 in
+        -r | --revision )
+            shift
+            revision=$1
+            ;;
+        -t | --target_arch )
+            shift
+            # Valid target architectures : arm, arm64, ia32, x64, mipsel
+            target_arch=$1
+           ;;
+        -h | --help )
+            usage
+            exit
+            ;;
+        * )
+            usage
+            exit 1
+    esac
+    shift
+done
+
+[ -n "$revision" ] && REVISION=" -r $revision"
+[ -n "$target_arch" ] && TARGET_ARCH="target_arch=$target_arch"
+
 pushd $SCRIPT_ABS_PATH > /dev/null
 
 # Check for updates
@@ -41,13 +74,15 @@ if [[ ! -f $FAIL_MARK && -d "src" ]]; then
 fi
 
 # Update
-gclient sync || \
+gclient sync $REVISION || \
 echo "*** Probably critical error ***"
 
 pushd "src" > /dev/null
 source ./build/android/envsetup.sh && \
-export GYP_DEFINES="OS=android $GYP_DEFINES" && \
+export GYP_DEFINES="build_with_libjingle=1 build_with_chromium=0 libjingle_java=1 enable_tracing=1 OS=android $TARGET_ARCH $GYP_DEFINES" && \
 gclient runhooks && \
+echo "Using following GYP_DEFINES: $GYP_DEFINES"
+
 ninja -C out/Debug libjingle_peerconnection_jar || \
 fail
 popd > /dev/null # "src"
