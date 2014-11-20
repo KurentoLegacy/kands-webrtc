34a1cfb083e79a24680442fd01943e09d182bc42
Javier Lopez <fjlopez@gsyc.es>
2014-11-20 17:46:27 +0100

    Do not check if code has been updated
    Change-Id: Ie7e21ef34c8b1d070640018add5599d1881b29bb

--- build.sh
+++ build.sh
@@ -5,20 +5,11 @@ source $SCRIPT_RELATIVE_PATH/utils.sh
 
 SCRIPT_ABS_PATH=$(get_abs_path)
 
-FAIL_MARK=last_execution_fails
-
-function fail {
-  cd $SCRIPT_ABS_PATH
-  echo "FAIL" && touch $FAIL_MARK && exit -1
-}
-
 function end {
   pushd $SCRIPT_ABS_PATH > /dev/null
 
   cp src/out/Debug/libjingle_peerconnection.jar . && \
-  cp src/out/Debug/libjingle_peerconnection_so.so . && \
-  rm -f $FAIL_MARK || \
-  fail
+  cp src/out/Debug/libjingle_peerconnection_so.so . &&
 
   popd > /dev/null # $SCRIPT_ABS_PATH
 
@@ -60,19 +51,6 @@ done
 
 pushd $SCRIPT_ABS_PATH > /dev/null
 
-# Check for updates
-if [[ ! -f $FAIL_MARK && -d "src" ]]; then
-  pushd "src" > /dev/null
-
-  DIFF=$(svn diff --summarize -rCOMMITTED:HEAD)
-  if [ -z "$DIFF" ]; then
-    echo "There is not any change";
-    end
-  fi
-
-  popd > /dev/null # "src"
-fi
-
 # Get depot_tools
 [ -d ./depot_tools ] || git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git || \
     { echo "Unable to download depot_tools"; exit 1; }
@@ -88,10 +66,7 @@ export GYP_DEFINES="build_with_libjingle=1 build_with_chromium=0 libjingle_java=
 gclient runhooks && \
 echo "Using following GYP_DEFINES: $GYP_DEFINES"
 
-ninja -C out/Debug libjingle_peerconnection_jar || \
-fail
+ninja -C out/Debug libjingle_peerconnection_jar
 popd > /dev/null # "src"
-
 popd > /dev/null # $SCRIPT_ABS_PATH
-
 end
